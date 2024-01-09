75c7891c3991b4332bbdbbce4dad4e9f7ba2ed06
Dan Baker <daniel.n.baker@gmail.com>
2024-01-08 21:43:14 -0800

    content/update-sponsor-section (#8)
    * Added a conditional to highlight the absense of sponsors in the sponsors section.
    
    * Found the SectionParagraph component.

--- src/components/common/Sponsors/Sponsors.svelte
+++ src/components/common/Sponsors/Sponsors.svelte
@@ -13,6 +13,10 @@
     : sponsors;
 </script>
 
+{#if randomizedSponsors.length === 0}
+<p>We have no {level} sponsors yet! Interested? See <a href="https://discourse.nixos.org/t/nixcon-north-america-sponsorship-tracks/37755"> this post</a>.</p>
+{:else}
 {#each randomizedSponsors as sponsor}
     <Sponsor {level} image={sponsor.image} alt={sponsor.alt} url={sponsor.url} extraClasses={sponsor.extraClasses}/>
 {/each}
+{/if}
